3e9b5abe6a458b2820101924d3f3a763363a9258
Özgür Çoban <cobanozgur55@gmail.com>
2023-08-24 18:01:08 +0300

    git problem solved  guess

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,8 +3,5 @@ export const config=()=>({
      mongodb_menu_url:process.env.MONGODB_MENU_URL,
      mongodb_consumer_url:process.env.MONGODB_CONSUMER_URL,
      mongodb_table_url:process.env.MONGODB_TABLE_URL,
-<<<<<<< HEAD
      mongodb_order_url:process.env.MONGODB_ORDER_URL,
-=======
->>>>>>> a1dfc20396f9052ae16443ae11cda08562089f89
 })
