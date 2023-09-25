eef67a28477ca881bcd84d69ae3eeecc268ff3f1
Özgür Çoban <cobanozgur55@gmail.com>
2023-09-25 19:28:22 +0300

    fix cors 4

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ImATeapotException } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService=app.get(ConfigService)
-  const PORT=configService.get<string>('port')
-
   const whitelist = [
     'http://https://restaurant-test-vite.netlify.app/consumer/scanQr',
     'http://https://restaurant-test-vite.netlify.app/consumer/enterName',
@@ -15,12 +12,34 @@ async function bootstrap() {
     'http://localhost:5173/consumer/scanQr',
 
   ];
+  const app = await NestFactory.create(AppModule, {
 
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin:   whitelist,
-    credentials: true,
+    cors: {
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      origin: function (origin, callback) {
+        if (!origin) {
+          callback(null, true);
+          return;
+        }
+        if (
+          whitelist.includes(origin) || // Checks your whitelist
+          !!origin.match(/yourdomain\.com$/) // Overall check for your domain
+        ) {
+          console.log('allowed cors for:', origin);
+          callback(null, true);
+        } else {
+          console.log('blocked cors for:', origin);
+          callback(new ImATeapotException('Not allowed by CORS'), false);
+        }
+      },
+    },
   });
+  const configService=app.get(ConfigService)
+  const PORT=configService.get<string>('port')
+
+
+
+
   await app.listen(PORT);
 }
 bootstrap();
