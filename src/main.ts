9f63feef98f7ceeca78127d3ab0baf475b85fe61
Özgür Çoban <cobanozgur55@gmail.com>
2023-09-25 19:26:04 +0300

    cors fix 3

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService=app.get(ConfigService)
   const PORT=configService.get<string>('port')
+
+  const whitelist = [
+    'http://https://restaurant-test-vite.netlify.app/consumer/scanQr',
+    'http://https://restaurant-test-vite.netlify.app/consumer/enterName',
+    'http://https://restaurant-test-vite.netlify.app/kitchen/admin',
+    'http://localhost:5173/kitchen/admin',
+    'http://localhost:5173/consumer/scanQr',
+
+  ];
+
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'https://restaurant-test-vite.netlify.app',
+    origin:   whitelist,
     credentials: true,
   });
   await app.listen(PORT);
