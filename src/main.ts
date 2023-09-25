702766feeb444b2610b9081dcf57046f6954053f
Özgür Çoban <cobanozgur55@gmail.com>
2023-09-25 19:24:10 +0300

    cors fix 2

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const configService=app.get(ConfigService)
   const PORT=configService.get<string>('port')
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    allowedHeaders: ['content-type'],
+    origin: 'https://restaurant-test-vite.netlify.app',
     credentials: true,
   });
   await app.listen(PORT);
